0eb39ff4c3eb40c081ccc1be413b8e245a88b653
Tarun Kumar <yadavatarun18@gmail.com>
2025-08-06 18:07:43 +0530

    Small change

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-import User from "./models/User"
+import User from "./models/User.js"
 import bcrypt from "bcrypt";
 import dotenv from "dotenv"
 import cors from "cors";
